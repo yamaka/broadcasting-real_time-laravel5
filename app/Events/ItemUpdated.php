82b0ba3b5d78e32d28348233925edfda5c73fb1e
diegofertr <diegonano09@gmail.com>
2017-04-06 19:12:44 -0400

    Adding Code to RealTime application laravel

---
+++ app/Events/ItemUpdated.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use App\Item;
+use App\Events\Event;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class ItemUpdated extends Event implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    public $id;
+    public $isCompleted;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Item $item
+     * @return void
+     */
+    public function __construct(Item $item)
+    {
+        $this->id = $item->id;
+        $this->isCompleted = (bool) $item->isCompleted;
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return ['itemAction'];
+    }
+}
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Real-time Todo App Example
+
+This is the starter code for the article that demostrates Laravel's broadcasting events functionality
+
+## Install Instructions
+
+* Clone repository
+
+    ```bash
+    git clone https://github.com/cwt137/l51-todo-app todo-app
+    ```
+
+    ```bash
+    cd todo-app
+    ```
+
+
+* Install Dependencies
+
+    ```bash
+    composer install
+    ```
+
+
+* Setup Database
+
+    ```bash
+    php artisan migrate
+    ```
+
+## Read The Article
+
+To learn how to use this starter code, read the article:
+
+http://www.sitepoint.com/real-time-apps-laravel-5-1-event-broadcasting/
