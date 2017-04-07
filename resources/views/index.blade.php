15aeefca933ee6ab70f8b630d16460d39b2d614c
diegofertr <diegonano09@gmail.com>
2017-04-07 17:14:36 -0400

    Bug fixes view index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Todo App Example</title>
+    <title>Broadcasting todo-app</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
